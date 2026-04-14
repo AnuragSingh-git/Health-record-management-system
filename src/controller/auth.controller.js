af51bd91a9a68d23f34bbfc7db07d729fea8f45d
Anurag Singh <anurag28singh88@gmail.com>
2026-04-14 22:56:02 +0530

    token and cookie funtionality added

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 import user from "../model/user.model.js";
+import bycrpt from "bcrypt"
+import jwt from "jsonwebtoken"
+import cookie from "cookie-parser"
+
 
 const register= async (req,res)=>{
+    try{
     const {name,email,password}=req.body
 
     if(!name||!email||!password){
@@ -13,20 +18,29 @@ const register= async (req,res)=>{
     if(userexist){
         return res.status(400).json({
             message: "user already exist"
-        }
-        )
+        })
     }
-    try{const usercreated=
-        await user.create({
+    const hash=await bycrpt.hash(password,10)
+    const usercreated= await user.create({
         name:name,
         email:email,
-        password:password
+        password:hash
+    })
+    const token=jwt.sign({
+        name,
+        email
+    },"MPcHQouySHaRd2aU4pK8efeMaiDogEL2MCySxAuDt3I",
+    {expiresIn:"1d"})
+
+    cookie("Token",token)
+    
+    res.status(201).json({
+        message:"user created",
+        user:usercreated,
+        Token:token
     })}
     catch(error){
         console.log(error)
     }
-    res.status(201).json({
-        message:"user created"
-    })
 }
 export default register
