b1506ed25db91bd167105162e407db965ee0e648
Anurag Singh <anurag28singh88@gmail.com>
2026-04-26 23:39:02 +0530

    fix code

--- src/routes/request.route.js
+++ src/routes/request.route.js
@@ -1,12 +1,12 @@
 import express from "express"
-import {gotrequest, seekrequest} from "../controller/request.controller.js"
+import {checkuserexist, gotrequest, seekrequest, seerecord} from "../controller/request.controller.js"
 import authmiddleware from "../middleware/auth.middleware.js"
 
 const requestrouter=express.Router()
 
-requestrouter.get("/search/:id",authmiddleware,)
+requestrouter.get("/search/:id",authmiddleware,checkuserexist)
 
-requestrouter.get("/data/:id",authmiddleware,)
+requestrouter.get("/data/:id",authmiddleware,seerecord)
 
 requestrouter.post("/send/id",authmiddleware,seekrequest)
 
