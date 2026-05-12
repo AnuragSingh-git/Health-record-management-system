d0064ea90c5678066999e639bfd99539fc80b83f
AnuragSingh-git <Anurag28singh88@gmail.com>
2026-05-12 23:03:23 +0530

    Update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='h-screen bg-[url("https://images.unsplash.com/photo-1641160923894-b1a80920187d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]'>
-      App test
+      App
     </div>
   )
 }
