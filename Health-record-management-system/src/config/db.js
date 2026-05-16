b4d59442f9f87b89d6f389086441e3a0fa2b00da
AnuragSingh-git <Anurag28singh88@gmail.com>
2026-05-16 18:53:29 +0530

    Update database

--- Health-record-management-system/src/config/db.js
+++ Health-record-management-system/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connecttodb = async ()=>{
     try{
         await mongoose.connect("mongodb://anurag28singh88_db_user:ouzrA9tqpy9jfN5o@ac-mvm2oxt-shard-00-00.pklm9ct.mongodb.net:27017,ac-mvm2oxt-shard-00-01.pklm9ct.mongodb.net:27017,ac-mvm2oxt-shard-00-02.pklm9ct.mongodb.net:27017/?ssl=true&replicaSet=atlas-14e5ud-shard-0&authSource=admin&appName=ProjectDB")
-        console.log("conected to database")
+        console.log("database is connected")
     }
     catch(error){
         console.log(error)
