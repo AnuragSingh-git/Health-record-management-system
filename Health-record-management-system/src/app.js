e665d4c055c69a00875457504beb6eb3e4df2c50
Anurag Singh <anurag28singh88@gmail.com>
2026-05-21 23:57:35 +0530

    register api added

--- Health-record-management-system/src/app.js
+++ Health-record-management-system/src/app.js
@@ -8,7 +8,10 @@ import cors from 'cors'
 const app=express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors(null))
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use('/api/auth',Router)
 app.use('/api/upload',uploadapi)
