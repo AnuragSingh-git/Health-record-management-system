9c2c1c079066de96fa08dd2cec2f060edd8b9b35
Anurag Singh <anurag28singh88@gmail.com>
2026-05-25 23:18:17 +0530

    Dasboard page

---
+++ frontend/src/components/DoctorDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const DoctorDashboa = () => {
+  return (
+    <div className='h-screen w-screen bg-blue-400 items-center justify-center flex'>
+      <div className='h-1/2 w-1/2 bg-amber-50 rounded-4xl border'>
+      </div>
+    </div>
+  )
+}
