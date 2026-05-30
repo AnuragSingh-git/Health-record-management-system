1fd84e189bc93978aa55dd0224fddd259245c1a0
Anurag Singh <anurag28singh88@gmail.com>
2026-05-31 00:28:53 +0530

    request controller

--- frontend/src/components/DoctorDashboard.jsx
+++ frontend/src/components/DoctorDashboard.jsx
@@ -34,11 +34,11 @@ export const DoctorDashboard = () => {
     <div className='h-screen w-screen bg-blue-400 items-center justify-center flex'>
       <div className='h-1/2 w-1/2 flex flex-col bg-amber-50 rounded-4xl border items-center justify-center gap-2'>
         <h1 className='text-2xl font-bold p-2 rounded-xl bg-amber-400 border'>Welcome {user.name}</h1>
-        <div>
+        <div className='flex flex-col gap-2'>
         {user.role=="doctor"&&<div className='flex flex-col gap-2'><div className='text-center'>Search Patients</div>
         <input type="text" onChange={(e)=>{setuserid(e.target.value)}} className='border' placeholder="Search patients..." />
         <button onClick={searchhandle} className='bg-gray-600 rounded-xl p-2'>Search</button></div>}
-        {searcheduser.patientname&&<div className='text-center'>Patient Name: {searcheduser.patientname}</div>}
+        {searcheduser.patientname&&<div className='text-center border'>Patient Name: {searcheduser.patientname}</div>}
         </div>
         </div>
       </div>
