deb6a8ef915a96e6f3ac8c6a4b35e7f3809bc644
Anurag Singh <anurag28singh88@gmail.com>
2026-06-02 22:00:04 +0530

    react outer dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Register from './components/register.jsx'
 import Login from './components/Login.jsx'
 import { DoctorDashboard } from './components/DoctorDashboard.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='h-screen items-center justify-center bg-[url("https://images.unsplash.com/photo-1641160923894-b1a80920187d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]'>
-      <Register/>
-      <Login/>
-      <DoctorDashboard/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/> 
+          <Route path='/register' element={<Register/>}/> 
+          <Route path='/dashboard' element={<DoctorDashboard/>}/> 
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
