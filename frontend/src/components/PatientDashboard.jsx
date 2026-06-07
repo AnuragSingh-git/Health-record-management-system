ccd72bcfbf1f1acf12e037f89bea0e1f537a2f62
Anurag Singh <anurag28singh88@gmail.com>
2026-06-07 22:31:38 +0530

    new dashboard fix

--- frontend/src/components/PatientDashboard.jsx
+++ frontend/src/components/PatientDashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const PatientDashboard = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-blue-400">
-      <div>Patient Dashboard</div>
+      <div></div>
     </div>
   )
 }
