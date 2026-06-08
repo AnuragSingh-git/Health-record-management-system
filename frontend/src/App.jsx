1d7e1c115d842b3e9bfb56b6610535e4c84b9c27
Anurag Singh <anurag28singh88@gmail.com>
2026-06-08 22:20:28 +0530

    new dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div className='h-screen items-center justify-center bg-[url("https://images.unsplash.com/photo-1641160923894-b1a80920187d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login/>}/> 
+          <Route path='/Login' element={<Login/>}/> 
           <Route path='/register' element={<Register/>}/> 
           <Route path='/dashboard' element={<DoctorDashboard/>}/> 
           <Route path='/request/:id' element={<RequestPage/>}/>
--- frontend/src/components/PatientDashboard.jsx
+++ frontend/src/components/PatientDashboard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
+import api from '../services/api'
 
 export const PatientDashboard = () => {
+    const [User, setUser] = useState({})
+    useEffect(() => {
+        const fetchUser = async () => {
+        try{const userdata = await api.get('/api/auth/getuser')
+        if(!userdata){
+          return console.log("no user found")
+        }
+        setUser(userdata.data.user)
+      } catch (error) {
+        console.error("Error fetching user data:", error.response?.data || error.message)
+      }
+    }
+
+    fetchUser()
+  }, [])
+
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-blue-400">
-      <div></div>
+    <div className="h-screen w-screen flex flex-col gap-2 items-center justify-center bg-blue-400">
+      <div className="bg-white p-4 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold text-gray-800 text-center uppercase">{User.name}</h1>
+        <p className="text-gray-600">Welcome to your dashboard!</p>
+      </div>
+      <div className="bg-white p-4 rounded-lg shadow-lg justify-center items-center flex flex-col">
+        <h2 className="text-xl font-semibold text-gray-800">Your Health Records</h2>
+        <p className="text-gray-600">You can view and manage your health records here.</p>
+        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg">View Records</button>
+        <div className="mt-4 text-gray-600">You can also manage your permissions for doctors to access your records.</div>
+        <button className="mt-2 bg-green-500 text-white px-4 py-2 rounded-lg">Manage Permissions</button>
+      </div>
     </div>
   )
 }
