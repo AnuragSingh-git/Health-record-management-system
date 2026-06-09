a378da862bfc5910371e0348977632fb7c319341
Anurag Singh <anurag28singh88@gmail.com>
2026-06-09 23:37:51 +0530

    request controller

--- frontend/src/components/PatientDashboard.jsx
+++ frontend/src/components/PatientDashboard.jsx
@@ -19,19 +19,16 @@ export const PatientDashboard = () => {
     fetchUser()
   }, [])
 
-  const permissionhandle=()=>{
-    const fetchrequests = async () => {
+  const permissionhandle=async ()=>{
       try {
         const response = await api.get('/api/request/getrequests');
-        setgotrequests(response.data);
-        console.log(response.data);
+        setgotrequests(response.data.requestgot);
+        console.log(response.data.requestgot);
       } catch (error) {
         console.error('Error fetching permission requests:', error.response?.data || error.message);
       }
     };
 
-    fetchrequests();
-  }
 
   return (
     <div className="h-screen w-screen flex flex-col gap-2 items-center justify-center bg-blue-400">
@@ -45,14 +42,14 @@ export const PatientDashboard = () => {
         <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg">View Records</button>
         <div className="mt-4 text-gray-600">You can also manage your permissions for doctors to access your records.</div>
         <button onClick={permissionhandle} className="mt-2 bg-green-500 text-white px-4 py-2 rounded-lg">Manage Permissions</button>
-        <div><div className='bg-gray-200 rounded-xl px-4 py-2 mt-4'>Permission Requests</div>
-        <div></div>
-        {gotrequests.map((gotrequests,index)=>(
+        <div className='flex flex-col items-center justify-center'><div className='bg-gray-200 w-fit rounded-xl px-4 py-2 mt-4'>Permission Requests</div>
+        <div>
+        {gotrequests.map((requests,index)=>(
           <div key={index} className='bg-gray-100 rounded-lg p-2 mt-2 w-full'>
-            <div className='text-gray-800 font-semibold'>Doctor Name: {gotrequests[index].doctorname}</div>
-            <div className='text-gray-600'>Status: {gotrequests[index].status}</div>
+            <div className='text-gray-800 font-semibold'>Doctor Name: {requests.doctorid}</div>
+            <div className='text-gray-600'>Status: {requests.status}</div>
           </div>
-        ))}
+        ))}</div>
         </div>
       </div>
     </div>
