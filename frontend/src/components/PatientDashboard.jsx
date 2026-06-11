516bb53ce134303b6a0c8b692398cbbab4594723
Anurag Singh <anurag28singh88@gmail.com>
2026-06-11 19:49:31 +0530

    permission

--- frontend/src/components/PatientDashboard.jsx
+++ frontend/src/components/PatientDashboard.jsx
@@ -4,6 +4,7 @@ import api from '../services/api'
 export const PatientDashboard = () => {
     const [User, setUser] = useState({})
     const [gotrequests, setgotrequests] = useState([])
+    const [permission, setpermission] = useState(false)
     useEffect(() => {
         const fetchUser = async () => {
         try{const userdata = await api.get('/api/auth/getuser')
@@ -39,15 +40,18 @@ export const PatientDashboard = () => {
       <div className="bg-white p-4 rounded-lg shadow-lg justify-center items-center flex flex-col">
         <h2 className="text-xl font-semibold text-gray-800">Your Health Records</h2>
         <p className="text-gray-600">You can view and manage your health records here.</p>
-        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg">View Records</button>
+        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:scale-105 active:scale-90">View Records</button>
         <div className="mt-4 text-gray-600">You can also manage your permissions for doctors to access your records.</div>
-        <button onClick={permissionhandle} className="mt-2 bg-green-500 text-white px-4 py-2 rounded-lg">Manage Permissions</button>
+        <button onClick={permissionhandle} className="mt-2 bg-green-500 text-white px-4 py-2 rounded-lg hover:scale-105 active:scale-90">Manage Permissions</button>
         <div className='flex flex-col items-center justify-center'><div className='bg-gray-200 w-fit rounded-xl px-4 py-2 mt-4'>Permission Requests</div>
         <div>
         {gotrequests.map((requests,index)=>(
           <div key={index} className='bg-gray-100 rounded-lg p-2 mt-2 w-full'>
             <div className='text-gray-800 font-semibold'>Doctor Name: {requests.doctorid.name}</div>
             <div className='text-gray-600'>Status: {requests.status}</div>
+            {requests.status === 'pending' && (
+              <button className='mt-2 bg-blue-500 text-white px-4 py-1 rounded-lg hover:scale-105 active:scale-90'></button>
+            )}
           </div>
         ))}</div>
         </div>
