7987eb31dd3a36adabc2c5832e543ecb8400f028
Anurag Singh <anurag28singh88@gmail.com>
2026-06-16 18:54:52 +0530

    better route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://health-record-management-system-ho04.onrender.com',
+  withCredentials:true
 });
-api.defaults.withCredentials = true;
 
 export default api;
