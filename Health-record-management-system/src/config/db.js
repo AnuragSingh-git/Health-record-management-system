373193a81afb6cca7bbdf5539b7a4a85ed0a94cd
AnuragSingh-git <Anurag28singh88@gmail.com>
2026-06-16 21:11:28 +0530

    Change MongoDB connection to use MONGODB_URI
    Updated MongoDB connection string to use environment variable.

--- Health-record-management-system/src/config/db.js
+++ Health-record-management-system/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connecttodb = async ()=>{
     try{
-        await mongoose.connect("mongodb://anurag28singh88_db_user:ouzrA9tqpy9jfN5o@ac-mvm2oxt-shard-00-00.pklm9ct.mongodb.net:27017,ac-mvm2oxt-shard-00-01.pklm9ct.mongodb.net:27017,ac-mvm2oxt-shard-00-02.pklm9ct.mongodb.net:27017/?ssl=true&replicaSet=atlas-14e5ud-shard-0&authSource=admin&appName=ProjectDB")
+        await mongoose.connect("process.env.MONGODB_URI")
         console.log("connected to database")
     }
     catch(error){
