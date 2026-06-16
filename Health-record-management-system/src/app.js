fefedf8dc5540853918c416bc0dfe34823c2f62f
Anurag Singh <anurag28singh88@gmail.com>
2026-06-16 18:14:19 +0530

    github fixed

--- Health-record-management-system/src/app.js
+++ Health-record-management-system/src/app.js
@@ -9,7 +9,7 @@ const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://health-record-management-system-three.vercel.app",
     credentials:true
 }))
 
