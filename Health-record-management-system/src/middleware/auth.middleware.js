0b62d4fd39fd8ca8e4ac8924254b1f36fa3c64e3
AnuragSingh-git <Anurag28singh88@gmail.com>
2026-07-05 21:25:51 +0530

    middleware new filed added

--- Health-record-management-system/src/middleware/auth.middleware.js
+++ Health-record-management-system/src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ const authmiddleware=(req,res,next)=>{
     const cookie=req.cookies.Accesstoken
     if(!cookie){
         return res.status(401).json({
-            message:"No token found"
+            message:"No token"
         })
     }
     
